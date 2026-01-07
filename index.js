d7ac144dac7265e3c3eeb7d2bf39fb7067d02361
Muh. Alif Anhar <sultankautsar21@gmail.com>
2026-01-07 17:23:58 +0800

    setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) +1;
+    res.render('random', { num});
+})
+
+app.listen(8080, () => {
+    console.log(`listening on port http://localhost:8080`);
+});
